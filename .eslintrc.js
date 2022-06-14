5c05e34f9d499d83c9074b2e623530f5218b0dc4
mnseong <mskweon@icloud.com>
2022-06-14 17:00:59 +0900

    Initial settings

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin-node/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+}

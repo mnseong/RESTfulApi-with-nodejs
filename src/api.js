eb664107a0d55fa73dd91e7d7f92f4bcdf8c5155
mnseong <mskweon@icloud.com>
2022-06-16 06:11:01 +0900

    Implement RESTful API

---
+++ src/api.js
@@ -0,0 +1,105 @@
+// @ts-check
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+/**
+ * @typedef APIResponse
+ * @property {number} statusCode
+ * @property {*} body
+ */
+/**
+ * @typedef Route
+ * @property {RegExp} url
+ * @property {'GET' | 'POST'} method
+ * @property {(matches: string[], body: Object.<string, *> | undefined) => Promise<APIResponse>} callback
+ */
+
+const fs = require('fs')
+const DB_JSON_FILENAME = 'database.json'
+/** @returns {Promise<Post[]>} */
+async function getPosts() {
+  const json = await fs.promises.readFile(DB_JSON_FILENAME, 'utf-8')
+  return JSON.parse(json).posts
+}
+/** @param {Post[]} posts */
+async function savePosts(posts) {
+  const content = {
+    posts,
+  }
+  return fs.promises.writeFile(
+    DB_JSON_FILENAME,
+    JSON.stringify(content),
+    'utf-8'
+  )
+}
+/** @type {Route[]} */
+const routes = [
+  {
+    url: /^\/posts$/,
+    method: 'GET',
+    callback: async () => ({
+      // TODO: implement
+      statusCode: 200,
+      body: await getPosts(),
+    }),
+  },
+  {
+    url: /^\/posts\/([a-zA-Z0-9-_]+)$/, // TODO: Regexp로 고쳐야함
+    method: 'GET',
+    callback: async (matches) => {
+      const postId = matches[1]
+      if (!postId) {
+        return {
+          statusCode: 404,
+          body: 'Not found',
+        }
+      }
+      const posts = await getPosts()
+      const post = posts.find((_post) => _post.id === postId)
+      if (!post) {
+        return {
+          statusCode: 404,
+          body: 'Not found',
+        }
+      }
+      return {
+        statusCode: 200,
+        body: post,
+      }
+    },
+  },
+  {
+    url: /^\/posts$/,
+    method: 'POST',
+    callback: async (_, body) => {
+      if (!body) {
+        return {
+          statusCode: 400,
+          body: 'Ill-formed request.',
+        }
+      }
+      /** @type {string} */
+      const title = body.title
+      const newPost = {
+        id: title.replace(/\s/g, '_'),
+        title,
+        content: body.content,
+      }
+      const posts = await getPosts()
+      posts.push(newPost)
+      savePosts(posts)
+      return {
+        statusCode: 200,
+        body: newPost,
+      }
+    },
+  },
+]
+
+module.exports = {
+  routes,
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 // @ts-check
+
+const http = require('http')
+const { routes } = require('./api')
+
+const server = http.createServer((req, res) => {
+  async function main() {
+    const route = routes.find(
+      (route) =>
+        req.url &&
+        req.method &&
+        route.url.test(req.url) &&
+        route.method == req.method
+    )
+    if (!req.url || !route) {
+      res.statusCode = 404
+      res.end('Not found.')
+      return
+    }
+    const regexpResult = route.url.exec(req.url)
+    if (!regexpResult) {
+      res.statusCode = 404
+      res.end('Not found.')
+      return
+    }
+    /** @type {Object.<string, *> | undefined} */
+    const reqBody =
+      (req.headers['content-type'] === 'application/json' &&
+        (await new Promise((resolve, reject) => {
+          req.setEncoding('utf-8')
+          req.on('data', (data) => {
+            try {
+              resolve(JSON.parse(data))
+            } catch {
+              reject(new Error('Ill-formed json'))
+            }
+          })
+        }))) ||
+      undefined
+    // console.log(body)
+
+    // 여기서 부터는 Route가 있다고 TypeScript에서 판단
+    const result = await route.callback(regexpResult, reqBody)
+    res.statusCode = result.statusCode
+    if (typeof result.body === 'string') {
+      res.end(result.body)
+    } else {
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(result.body))
+    }
+  }
+  main()
+})
+const PORT = 4000
+server.listen(PORT, () => {
+  console.log('The server is listening at port: ${PORT}')
+})
